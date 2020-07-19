2a24fadf5e6a084fdd9b9cee385546b950215d12
jeremiahtenbrink@gmail.com <jeremiahtenbrink@gmail.com>
2020-07-19 12:01:24 -0600

    Added test for enqueue

--- src/tests/Queue.test.ts
+++ src/tests/Queue.test.ts
@@ -1,14 +1,26 @@
 import { Queue } from "../Queue";
 
-describe('tests Queue Class', () => {
+describe( 'tests Queue Class', () => {
   let q = null
-  beforeEach(() => {
+  beforeEach( () => {
     q = new Queue();
-  })
+  } )
   
-  it('tests the creation of the queue', () => {
-    expect(q).toBeInstanceOf(Queue);
-    expect(q.getSize()).toBe(0)
-    expect(q.isEmpty()).toBe(true)
-  })
-})
+  it( 'tests the creation of the queue', () => {
+    expect( q ).toBeInstanceOf( Queue );
+    expect( q.getSize() ).toBe( 0 )
+    expect( q.isEmpty() ).toBe( true )
+  } )
+  
+  it( 'checks the enqueue function', () => {
+    q.enqueue( 5 )
+    expect( q.getSize() ).toBe( 1 )
+    expect( q.dll.head.value ).toBe( 5 )
+    q.enqueue( 6 )
+    expect( q.getSize() ).toBe( 2 )
+    expect( q.dll.tail.value ).toBe( 6 )
+    q.enqueue( 7 )
+    expect( q.getSize() ).toBe( 3 )
+    expect( q.dll.tail.value ).toBe( 7 )
+  } )
+} )
